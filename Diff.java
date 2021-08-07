44baf3619132b1463ad8b0c4dd8df7f3affcec3a
arjunv123 <donrajarjun@gmail.com>
2021-08-07 13:12:14 +0530

    new changes

--- Diff.java
+++ Diff.java
@@ -2,7 +2,7 @@ public class Diff{
 	public static void main(String a[]){
 	int num1 = 5; int num2 = 15; int diff
 	diff= num1 + num2;
-	System.out.println("The Sum is:"+diff);
+	System.out.println("The Sum :"+diff);
 
 }
 }
